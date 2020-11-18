f1d985d76b34a5692a8b54d11592524eae97ed6f
nguyentrongnhat <nguyentrongnhat240700@gmail.com>
2020-11-18 18:16:08 +0700

    rebuild table 'country'

---
+++ src/DataFixtures/CountryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Country;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Finder\Finder;
+
+class CountryFixtures extends Fixture
+{
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $objectManager)
+    {
+        $finder = new Finder();
+        $finder->in(__DIR__, '/DataFixtures/sql');
+        $finder->name('country.sql');
+
+        foreach ($finder as $file) {
+            $content = $file->getContents();
+
+            $stmt = $this->container->get('doctrine.orm.entity_manager')->getConnection()->prepare($content);
+            $stmt->execute();
+        }
+    }
+}
