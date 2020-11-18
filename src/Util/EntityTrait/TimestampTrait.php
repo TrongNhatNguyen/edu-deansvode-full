298c1ddad121aff6fefa525834dd0de9e5d1b21e
thquan25 <thquan25@gmail.com>
2020-11-18 11:56:27 +0700

    Add file TimestampTrait inside EntityTrait

---
+++ src/Util/EntityTrait/TimestampTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Util\EntityTrait;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait TimestampTrait
+{
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
