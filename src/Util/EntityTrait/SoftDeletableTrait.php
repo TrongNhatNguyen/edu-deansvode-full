e93690e4d5e1b883336b28cedb2cf7b42e4cba1f
thquan25 <thquan25@gmail.com>
2020-11-18 12:08:40 +0700

    Add SoftDeletableTrait inside EntityTrait

---
+++ src/Util/EntityTrait/SoftDeletableTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Util\EntityTrait;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class SoftDeletableTrait
+{
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $deletedAt;
+
+    public function setDeletedAt(\DateTime $datetime)
+    {
+        $this->deletedAt = $datetime;
+    }
+
+    public function getDeletedAt(): ?\DateTime
+    {
+        return $this->deletedAt;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->deletedAt instanceof \DateTimeInterface;
+    }
+
+    public function recover()
+    {
+        $this->deletedAt = null;
+    }
+}
